8fc200639f7c42f2073c30c5ac4ba0ef64a178ab
Alexandre <Alexandrefer1231@gmail.com>
2024-03-26 14:01:52 -0300

    feat: change base framework

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-6 lg:p-12">
+      <div className="w-full border border-red-300 ">
+        oi
+      </div>
+    </main>
+  );
+}
